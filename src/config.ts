91c71592e0ef8e0ac9dae8583f1b7df32c9bba05
夏沫花火zzz <xmcn@1l1.icu>
2024-09-02 03:08:23 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   site: {
-    icon: 'https://blog.1l1.icu/images/avatar.jpg',
-    title: 'Muska Ami DN42 Looking Glass'
+    icon: 'https://muska.zip/favicon.ico',
+    title: 'Muska Network DN42 Looking Glass'
   },
   full_url: 'https://api.looking-glass.dn42.1l1.icu',
   api_url: 'https://api.looking-glass.dn42.1l1.icu/api/'
